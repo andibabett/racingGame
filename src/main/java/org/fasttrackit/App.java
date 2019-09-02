6f2da3e3754279a3df05e2983d8921bc3735d06c
andibabett <andi_babett@yahoo.com>
2019-09-02 20:02:35 +0300

    studied classes variables, primitive data types and aggregation
    studied inheritance

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -29,6 +29,12 @@ public class App
         System.out.println(car.travelDistance);
         System.out.println(car.running);
 
+        System.out.println(car.engine);
+
+        car.accelerate(60, 1);
+
+
+
         Car car2 = new Car();
         car2.name = "Golf";
         car2.fuelLevel = 50;
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -12,4 +12,30 @@ public class Vehicle {
     boolean running;
 
 
+    //semnatura metodei
+    public double accelerate(double speed, double durationInHours) {
+        //corpul metodei/implementarea metodei
+        //sout+enter
+        System.out.println(name + "is accelerating with " + speed + "km/h for" + durationInHours + " h.");
+
+        double distance = speed + durationInHours;
+
+        System.out.println("Distance: " + distance);
+
+        travelDistance = travelDistance + distance;
+        //acestea 2 sunt la fel, a doua este prescurtare
+        // travelDistance += distance;
+
+        double usedFuel = distance * mileage / 100;
+        System.out.println("Used fuel: " + usedFuel);
+        fuelLevel -= usedFuel;
+        System.out.println("Remaining fuel" + fuelLevel);
+
+
+        return distance;
+
+
+    }
+
+
 }
